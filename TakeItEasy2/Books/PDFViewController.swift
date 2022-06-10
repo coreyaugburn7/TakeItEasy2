b35ddbba65960acc855ebfcf0e6623bccf26b47b
ImmortalSugimoto <leeahmed74@yahoo.com>
2022-06-10 10:24:28 -0500

    i love pizza

---
+++ TakeItEasy2/Books/PDFViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PDFViewController.swift
+//  TakeItEasy2
+//
+//  Created by Liban Abdinur on 6/10/22.
+//
+
+import UIKit
+
+class PDFViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    //i love pizza 
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
